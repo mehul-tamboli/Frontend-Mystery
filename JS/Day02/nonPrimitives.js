2ad19eb024a24d222c040eb7980ff143e17c256e
mehul-tamboli <mehultamboli73@gmail.com>
2025-12-12 09:43:51 +0530

    updated

--- JS/Day02/nonPrimitives.js
+++ JS/Day02/nonPrimitives.js
@@ -14,3 +14,26 @@ console.log(typeof username);
 console.log(username.firstName);
 
 console.log("isnew " + username.isLoggedin);
+
+
+let student1 ={
+    rollNo: 40,
+    isPassed:true,
+    div:"B"
+}
+console.log(student1);
+console.log(typeof student1);
+
+
+student1.rollNo=41;
+console.log(student1);
+
+let obj1 = {
+    "big color ": "Red",
+    height : 41,
+    isBig : true
+}
+console.log(obj1['big color ']); //access for space variable using square bracket
+
+let today = new Date(); //date is object
+console.log(today.getDate);
